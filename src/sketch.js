d64b9ab8ae7b5ea59d4913329b03bf690beb1b40
ASzczep12 <134708064+ASzczep12@users.noreply.github.com>
2023-06-14 15:15:15 +0200

    Dodałam opis kodu

--- src/sketch.js
+++ src/sketch.js
@@ -1,5 +1,6 @@
 // Szerokośc paska z kolorem 20 pikseli
 const barWidth = 20;
+// Poprzedni pasek (aby nie rysować go ponownie)
 let lastBar = -5;
 // Zapamientywamy poprzednie localizacje dla myszy
 let mouse_trail = [];
@@ -10,7 +11,7 @@ function setup() {
   // Zmiana na tryb kolorów HSB
   // (Tryb, Hue, Saturation, Brightness)
   colorMode(HSB, width, height, 100);
-  // Brak krawędzi - samo wypełnienie bez obwodzenia
+  // Bez konturu
   noStroke();
 }
 
@@ -18,34 +19,40 @@ function draw() {
 
   // Który pasek = pozycja myszy po X / szerokość paska
   let whichBar = mouseX / barWidth;
-  // 
+  // Jeśli pasek jest inny niż poprzedni
   if (whichBar !== lastBar) {
+    // Wybierz kolor na podstawie pozycji myszy
     let barX = whichBar * barWidth;
+    // Ustaw kolor wypełnienia na podstawie pozycji myszy
     fill(barX, mouseY, 66);
+    // Narysuj prostokąt na podstawie pozycji myszy
     rect(barX, 0, barWidth, height);
+    // Zapisz poprzedni pasek jako obecny
     lastBar = whichBar;
   }
 
-  // Add a point to the end of the trail at the mouse position
+  // Dodaj aktualną pozycję myszy do tablicy
   mouse_trail.push(new p5.Vector(mouseX, mouseY));
 
-  // If the trail gets too long, remove the first (oldest) point
+  // Jeśli ścieżka jest zbyt długa, usuń pierwszy (najstarszy) punkt
   if (mouse_trail.length > 30) {
+    // Usuń pierwszy element z tablicy
     mouse_trail.shift();
   }
 
-  // Draw the trail
+  // Narysuj ścieżkę
   for (let i = 0; i < mouse_trail.length; i++) {
-    // Trail point
+    // Pobierz punkt z tablicy
     let p = mouse_trail[i];
 
-    // Set fill color for circle to magenta
+    // Wybierz kolor na podstawie pozycji myszy
     let c = color('magenta');
     fill(c);
 
-    // The trail is smaller at the beginning,
-    // and larger closer to the mouse
+    // Rozmiar ścieżki jest mniejszy na początku,
+    // a większy bliżej myszy
     let size = 40.0 * i / mouse_trail.length;
+    // Narysuj koło w punkcie
     circle(p.x, p.y, size);
   }
 }
